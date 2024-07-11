a24535a78ab6860459dea8020116cebdfa830129
husham35 <husham35@gmail.com>
2024-07-11 12:02:33 +0000

    corrected db path

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+import os
+
 class Config:
+	BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 	SECRET_KEY = 'MYSUPERSECRETKEY'
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///pfm_jinja2'
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR ,'app/pfm_jinja2')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
